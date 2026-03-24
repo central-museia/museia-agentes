2937a7d0b94c1fa098a6f48daac1eecf2c026182
central-museia <agents.museia@gmail.com>
2026-03-24 12:31:01 -0300

    Update pagamentos.py

--- core/pagamentos.py
+++ core/pagamentos.py
@@ -1,1 +1,12 @@
 # integração com pagamento (Payhip)
+import streamlit as st
+
+def exibir_login():  # <--- O nome tem que ser EXATAMENTE este
+    st.title("🍿 Entrar na MuseIA")
+    token = st.text_input("Digite seu Token de Acesso", type="password")
+    if st.button("ASSISTIR AGORA"):
+        if token == "teste123":
+            st.session_state["logado"] = True
+            st.rerun()
+        else:
+            st.error("Token inválido.")
